29ffab2909720e5cefab3cfc6dc3aa67909c75c0
Ibsy Matute <ibsy@rocketmail.com>
2022-09-06 11:55:42 -0500

    add ejs template engine and set up express aspp to use ejs

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+// bring in express
 const express = require('express')
+// create the app
 const app = express()
+// set up server port
 const port = 3000
+
+app.set('view engine', 'ejs')
 
 // app is now our application
 // has methods, get, post, put, delete
 // two arguments, url and callback function(two arguments as well, will always be request and response)
 app.get('/', (req, res) => { 
-    res.send('Hello World!!')
+    res.render('index')
 }) 
+
+app.get('/abc', (req, res) => {
+    res.send('ABC!')
+})
 
 
 app.listen(port, () => {
