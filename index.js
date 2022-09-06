fb8603e392591985a9d23e84983eb9ee3a5b834f
Ibsy Matute <ibsy@rocketmail.com>
2022-09-06 15:01:20 -0500

    added middleware function

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 
+app.use(logger) // uses logger function before anything else
+
 // app is now our application
 // has methods, get, post, put, delete
 // two arguments, url and callback function(two arguments as well, will always be request and response)
 app.get('/', (req, res) => { 
+    console.log(req.test) // we get req.test value from logger function
     res.render('index', { firstName: 'Brian', lastName: 'Stanton' }) // can take in props that can be used in ejs file being rendered 'index'
 }) 
 
@@ -42,6 +45,7 @@ app.get('/users/:id', (req, res) => {
     for (let user of users){
         if (user.id == id){
             res.render('user', { user })
+            return
         }
     }
     res.send({error: `User with id ${id} does not exist`})
@@ -51,6 +55,12 @@ app.get('/test/:testId/:testName/:abc/:xyz', (req, res) => {
     console.log(req.params)
     res.send('Test')
 })
+
+function logger(req, res, next){
+    console.log(req.originalUrl) // one of the req properties originalUrl
+    req.test = 123 // .test is a newly created property we just created that is sent to the next() request
+    next()
+}
 
 
 app.listen(port, () => {
